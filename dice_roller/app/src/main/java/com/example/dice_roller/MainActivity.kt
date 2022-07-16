65c807c9ea31df32a8147cc1481fccd7c83fb5db
vjdchvz <victorjhondechavez@gmail.com>
2022-07-16 11:57:35 +0800

    Lucky number features

--- dice_roller/app/src/main/java/com/example/dice_roller/MainActivity.kt
+++ dice_roller/app/src/main/java/com/example/dice_roller/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+//button code
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
             rollButton.setOnClickListener { rollDice() }
@@ -26,7 +26,9 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         // Create new Dice object with 6 sides and roll it
         val dice = Dice(6)
+        //dice is for number sides,roll is for the randomizer
         val diceRoll = dice.roll()
+
 
         // Update the screen with the dice roll
         val diceImage: ImageView = findViewById(R.id.imageView)
@@ -40,6 +42,20 @@ class MainActivity : AppCompatActivity() {
             5 -> diceImage.setImageResource(R.drawable.dice_5)
             6 -> diceImage.setImageResource(R.drawable.dice_6)
         }
+
+        //determine which message will print based on rollResult
+        val dickey: ImageView = findViewById(R.id.imageView2)
+
+
+        when (diceRoll) {
+            //lucky number
+            5 -> dickey.setImageResource(R.drawable.img1)
+            //unlucky number
+            1 -> dickey.setImageResource(R.drawable.img2)
+            // try again
+            else -> dickey.setImageResource(R.drawable.img3)
+        }
+
 
     }
 }
